4d891c312c2cd6420b54059a5f70e9f9e958b25b
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2025-03-20 19:47:24 -0700

    jhbbkjb

---
+++ main.py
@@ -0,0 +1,11 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "neurum"
+    page.bgcolor = "white"
+
+    title=ft.Container(ft.Text('neurum', color="black", size=80), padding=ft.Padding(left=100, right= 100, top=100, bottom=100))
+
+    page.add(title)
+
+ft.app(main)
