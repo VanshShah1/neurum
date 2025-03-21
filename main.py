53928889abacca0d311d4c4ee71657beebbf1d25
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2025-03-20 19:51:01 -0700

    fevvdvdv

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main(page: ft.Page):
     page.title = "neurum"
     page.bgcolor = "white"
 
-    title=ft.Container(ft.Text('neurum', color="black", size=80), padding=ft.Padding(left=100, right= 100, top=100, bottom=100))
+    title=ft.Container(ft.Text('neurum', color="black", size=80), padding=ft.Padding(left=500, right= 100, top=100, bottom=100))
 
     page.add(title)
 
