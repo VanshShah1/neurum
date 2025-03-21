0a66842fa689b480d18aac4bfdf3d4d626756696
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2025-03-20 20:04:22 -0700

    scjhsfsf

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "neurum"
-    page.bgcolor = "white"
 
-    title=ft.Container(ft.Text('neurum', color="black", size=80), padding=ft.Padding(left=500, right= 100, top=100, bottom=100))
-
-    page.add(title)
+    page.title="neurum"
+    page.bgcolor="white"
+    header=ft.Text("neurum", color="black", size=100)
+    prompt_field=ft.CupertinoTextField(placeholder_text="ask neurum...", bgcolor=ft.Colors.GREY_900)
+    page.add(header, prompt_field)
 
 ft.app(main)
