74bf1d0afb4159caad09e46f7b84f93c51edf19b
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2025-03-20 20:17:36 -0700

    sffsf

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def main(page: ft.Page):
 
     page.title="neurum"
     page.bgcolor="white"
-    header=ft.Text("neurum", color="black", size=100)
-    prompt_field=ft.CupertinoTextField(placeholder_text="ask neurum...", bgcolor=ft.Colors.GREY_900)
+    header=ft.Container(ft.Text("neurum", color="black", size=100), alignment=ft.Alignment(x=0.5, y=0.5))
+    prompt_field=ft.CupertinoTextField(placeholder_text="ask neurum...", bgcolor=ft.Colors.GREY_300)
     page.add(header, prompt_field)
 
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER)
