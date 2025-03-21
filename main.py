349765cacf58de52286ef60351d293dea8c49a13
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2025-03-20 21:22:24 -0700

    esfsf

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import Neutron
+from g4f.client import Client
+import re
 
-win = Neutron.Window("Example", size=(600, 400), css="styles.css")
+client = Client()
+
+win = Neutron.Window("Neurum", size=(600, 400), css="styles.css")
 win.display(file="index.html")
 
+def onSearch():
+    query = win.getElementById("searchInput").value
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": query}]
+        )
+    content = response.choices[0].message.content
+    win.getElementById("result").innerHTML = f"Search result for: {content}"
 
-def onClick():
-    win.getElementById("title").innerHTML = "Hello:" + win.getElementById("search").value
-
-win.getElementById("submit").addEventListener("click", Neutron.event(onClick))
+win.getElementById("searchButton").addEventListener("click", Neutron.event(onSearch))
 
 win.show()
