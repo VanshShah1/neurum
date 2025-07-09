4d57758ae79b346e4a26dccf6105d242085118c9
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2025-07-09 15:39:08 -0700

    dfvdv

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { appWindow } from '@tauri-apps/api/window'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <div onMouseDown={() => appWindow.startDragging()}>
       <h1>Hello World</h1>
-    </>
+    </div>
   )
 }
 
